f99a32701f52a9b8df4be51bba29407fb01e075e
Hawoly Ba <hawolyba@gmail.com>
2019-05-24 20:22:09 +0200

    Change breakpoints

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 import './scss/main.scss'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
