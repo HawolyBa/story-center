3ef12c5966255217152558de482ad3075f00e0c1
Hawoly Ba <hawolyba@gmail.com>
2023-03-10 09:40:46 +0100

    Delete src/config directory

--- src/config/keys.js
+++
@@ -1,4 +0,0 @@
-export const pexelsAPI = "563492ad6f917000010000019c7d5c4fd9854b599f57f533f212b5ce"
-export const editorKey = "7iarikzmca3h41njon0yl1zn09xs3eyfjbnknvoggrgs1w9j"
-export const siteName = "Story Center"
-export const slogan = "Venez comme vous êtes"
